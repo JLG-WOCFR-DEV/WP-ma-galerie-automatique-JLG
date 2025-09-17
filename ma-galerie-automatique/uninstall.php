a88929c5a6872480fe4df82b619f952499548586
Jewoc_DEV <jerome.legousse@gmail.com>
2025-09-17 22:49:35 +0200

    Handle uninstall cleanup across multisite

--- ma-galerie-automatique/uninstall.php
+++ ma-galerie-automatique/uninstall.php
@@ -1,1 +1,18 @@
+<?php
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit;
+}
 
+if ( is_multisite() ) {
+    $site_ids = get_sites( [ 'fields' => 'ids' ] );
+
+    foreach ( $site_ids as $site_id ) {
+        switch_to_blog( $site_id );
+        delete_option( 'mga_settings' );
+        restore_current_blog();
+    }
+
+    return;
+}
+
+delete_option( 'mga_settings' );
