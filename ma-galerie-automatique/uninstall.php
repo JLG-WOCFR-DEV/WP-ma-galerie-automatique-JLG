41977c3d29edab228ae9310221e71eafb97b191c
Jewoc_DEV <jerome.legousse@gmail.com>
2025-09-21 02:06:59 +0200

    Ensure multisite uninstall removes all mga_settings options

--- ma-galerie-automatique/uninstall.php
+++ ma-galerie-automatique/uninstall.php
@@ -4,7 +4,12 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 }
 
 if ( is_multisite() ) {
-    $site_ids = get_sites( [ 'fields' => 'ids' ] );
+    $site_ids = get_sites(
+        [
+            'fields' => 'ids',
+            'number' => 0,
+        ]
+    );
 
     foreach ( $site_ids as $site_id ) {
         switch_to_blog( $site_id );
