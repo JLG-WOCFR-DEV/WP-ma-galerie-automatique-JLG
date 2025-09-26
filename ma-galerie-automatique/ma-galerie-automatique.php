d4d842cdf22b483cd4a09699a365796c536b9888
Jewoc_DEV <jerome.legousse@gmail.com>
2025-09-26 11:37:18 +0200

    Ensure swiper asset sources option autoload remains disabled

--- ma-galerie-automatique/ma-galerie-automatique.php
+++ ma-galerie-automatique/ma-galerie-automatique.php
@@ -101,7 +101,7 @@ function mga_refresh_swiper_asset_sources() {
     if ( false === $existing_sources ) {
         add_option( 'mga_swiper_asset_sources', $sources, '', 'no' );
     } else {
-        update_option( 'mga_swiper_asset_sources', $sources, false );
+        update_option( 'mga_swiper_asset_sources', $sources, 'no' );
     }
 
     return $sources;
